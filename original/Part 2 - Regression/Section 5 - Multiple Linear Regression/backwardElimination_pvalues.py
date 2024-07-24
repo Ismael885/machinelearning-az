8fbfddac4f8aad261c4312cf55cb2e95d86bf9ed
IsmaelCarrizo <ismacarrizo10@gmail.com>
2024-07-24 14:00:58 -0300

    backward_elimination_strategies

---
+++ original/Part 2 - Regression/Section 5 - Multiple Linear Regression/backwardElimination_pvalues.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 16 23:25:14 2024
+
+@author: Usuario
+"""
+
+# Eliminacion hacia atras utilizando p-valores
+
+import statsmodels.formula.api as sm
+def backwardElimination(x, sl):    
+    numVars = len(x[0])    
+    for i in range(0, numVars):        
+        regressor_OLS = sm.OLS(y, x.tolist()).fit()        
+        maxVar = max(regressor_OLS.pvalues).astype(float)        
+        if maxVar > sl:            
+            for j in range(0, numVars - i):                
+                if (regressor_OLS.pvalues[j].astype(float) == maxVar):                    
+                    x = np.delete(x, j, 1)    
+    regressor_OLS.summary()    
+    return x 
+ 
+SL = 0.05
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+X_Modeled = backwardElimination(X_opt, SL)
